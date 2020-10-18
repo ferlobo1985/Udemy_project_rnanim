198bd79589a84d3852b1df4478a5e4318f3a7df8
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-18 01:44:00 -0300

    Parallel and sequence animations

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import {Header} from 'react-native-elements'
 
 import AnimOne from './src/anim_one';
-
+import AnimTwo from './src/anim_two';
 const App = () =>  {
   return (
     <View>
       <Header centerComponent={{ text: 'Anim project', style: { color: '#fff' } }} />
-      <AnimOne/>
+      <AnimTwo/>
     </View>
   );
 }
